922d72361ba8fc797e43025714dfc47e76cb726b
Eze Ringuelet <eze87lp@gmail.com>
2017-05-09 18:59:16 -0300

    agregando funcionalidad al ABMpublicaciones

--- Angular/carteleras/app/js/ABMpublicaciones/controllers/ABMpublicaciones.ctrl.js
+++ Angular/carteleras/app/js/ABMpublicaciones/controllers/ABMpublicaciones.ctrl.js
@@ -2,7 +2,7 @@ angular.module('myapp.ABMpublicaciones')
 .controller('ABMpublicacionesCtrl', function($scope, $state, $stateParams, CarteleraService, LoginService, $rootScope){	
 	$scope.usuario = angular.fromJson(localStorage.getItem('usuario'));
 
-	/*$scope.cargarCarteleras = function() {
+	$scope.cargarCarteleras = function() {
 		CarteleraService.getCarteleras().then(function(response){
 	  		$scope.carteleras = response.data;
 		}, 
@@ -11,6 +11,15 @@ angular.module('myapp.ABMpublicaciones')
 	  	})
  	};
 
-	$scope.cargarCarteleras();	*/
+	$scope.cargarCarteleras();
+
+	$scope.cargarPublicaciones = function(){
+		CarteleraService.getCartelera($scope.selected)
+		.then(function(response){
+			  //$scope.anioActivo = id;
+			$scope.cartelera = response.data;
+			console.log(response.data);
+		});
+	};
 
 });
