fc09bd452a888ee2f64d8b16c847a2d52602d992
lucholafrazia <lucholafrazia@gmail.com>
2017-05-18 00:40:47 -0300

    Funciona script para foto perfil

--- Angular/carteleras/app/upload.php
+++ Angular/carteleras/app/upload.php
@@ -1,10 +1,30 @@
 <?php 
-  $path = '/js/cartelera/img/'. $_POST['file']['name'];
-  if (move_uploaded_file($_POST['file']['tmp_name'], $path)) {
-    echo 'file uploaded';
-  }
-  else{
-    echo 'file error';
-  }
+	$extensionesPermitidas = array("jpg","jpeg","gif","png");
+	$extensionArchivo = end(explode(".",$_FILES["file"]["name"]));
+	$dir = 'js/cartelera/img/';
+	var_dump($_FILES);
+	//echo "$extensionArchivo";
+	echo $dir . basename($_FILES['file']['name']);
+	if ((($_FILES['file']['type'] == "image/gif")
+		|| ($_FILES['file']['type'] == "image/jpeg")
+		|| ($_FILES['file']['type'] == "image/png")
+		|| ($_FILES['file']['type'] == "image/jpg")) 
+		&& ($_FILES['file']['size'] < 80000)
+		&& in_array($extensionArchivo, $extensionesPermitidas))
+	{
+			if (move_uploaded_file($_FILES['file']['tmp_name'], $dir . basename($_FILES['file']['name'])))
+			{
+				echo "Archivo: " . $_FILES["file"]["name"] ."<br>";
+				echo "Tipo: " . $_FILES["file"]["type"] ."<br>";
+				echo "Tamaño " . $_FILES["file"]["size"] ."<br>";
+				echo "Guardado en " . $_FILES["file"]["tmp_name"] ."<br>";
+			}
+			else {
+				echo "Error: " . $_FILES["file"]["error"] . "<br>";
+			}	
+	}
+	else {
+		echo "Archivo inválido";
+	}
   
 ?>
