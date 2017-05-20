8e01b0be2e07704f146fc2805057c87cb948ba75
Maxi Mendivil <maximendivil22@gmail.com>
2017-05-20 14:52:30 -0300

    ABM publicaciones - Perfiles publicador, profesor y admin

--- Angular/carteleras/app/js/ABMpublicaciones/controllers/ABMpublicaciones.ctrl.js
+++ Angular/carteleras/app/js/ABMpublicaciones/controllers/ABMpublicaciones.ctrl.js
@@ -3,23 +3,35 @@ angular.module('myapp.ABMpublicaciones')
 	$scope.usuario = angular.fromJson(localStorage.getItem('usuario'));
 
 	$scope.cargarCarteleras = function() {
-		CarteleraService.getCarteleras().then(function(response){
+		if ($scope.usuario.rol == 1) {
+			CarteleraService.getCarteleras().then(function(response){
+		  		$scope.carteleras = response.data;
+			}, 
+			function(response) {
+		      console.log("algo anduvo mal");
+		  	});
+		}
+		else {
+			CarteleraService.getCartelerasHabilitadas($scope.usuario.id).then(function(response){
 	  		$scope.carteleras = response.data;
-		}, 
-		function(response) {
-	      console.log("algo anduvo mal");
-	  	})
+			}, 
+			function(response) {
+	      		console.log("algo anduvo mal");
+		  	});
+		}		
  	};
 
 	$scope.cargarCarteleras();
 
 	$scope.cargarPublicaciones = function(){
-		CarteleraService.getCartelera($scope.selected)
-		.then(function(response){
-			  //$scope.anioActivo = id;
-			$scope.cartelera = response.data;
-			console.log(response.data);
-		});
+		if ($scope.selected != ""){
+			CarteleraService.getCartelera($scope.selected)
+			.then(function(response){
+				  //$scope.anioActivo = id;
+				$scope.publicaciones = response.data;
+				console.log(response.data);
+			});
+		}		
 	};
 
 });
